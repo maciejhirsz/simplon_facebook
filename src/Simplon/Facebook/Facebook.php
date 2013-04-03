54678456ff8336f7ed9bf1ec77c623f3af65c103
Tino Ehrich <ehrich@efides.com>
2013-04-03 16:43:19 +0200

    new implementation based on our demands. kicked FB's SDK

--- src/Simplon/Facebook/Facebook.php
+++ src/Simplon/Facebook/Facebook.php
@@ -1,207 +1,178 @@
 <?php
 
-  namespace Simplon\Facebook;
+    namespace Simplon\Facebook;
 
-  class Facebook
-  {
-    /** @var \Facebook */
-    private $_facebookSdk;
-
-    private $_appId;
-    private $_appSecret;
-    private $_appPermissions;
-
-    // ##########################################
-
-    /**
-     * @param $appId
-     * @return Facebook
-     */
-    public function setAppId($appId)
+    class Facebook
     {
-      $this->_appId = $appId;
+        private $_appId;
+        private $_appSecret;
+        private $_appPermissions;
 
-      return $this;
+        // ##########################################
+
+        /**
+         * @param $appId
+         * @return Facebook
+         */
+        public function setAppId($appId)
+        {
+            $this->_appId = $appId;
+
+            return $this;
+        }
+
+        // ##########################################
+
+        /**
+         * @return mixed
+         */
+        public function getAppId()
+        {
+            return $this->_appId;
+        }
+
+        // ##########################################
+
+        /**
+         * @param $appSecret
+         * @return Facebook
+         */
+        public function setAppSecret($appSecret)
+        {
+            $this->_appSecret = $appSecret;
+
+            return $this;
+        }
+
+        // ##########################################
+
+        /**
+         * @return mixed
+         */
+        public function getAppSecret()
+        {
+            return $this->_appSecret;
+        }
+
+        // ##########################################
+
+        /**
+         * @param array $appPermissions
+         * @return Facebook
+         */
+        public function setAppPermissions(array $appPermissions)
+        {
+            $this->_appPermissions = $appPermissions;
+
+            return $this;
+        }
+
+        // ##########################################
+
+        /**
+         * @return mixed
+         */
+        public function getAppPermissions()
+        {
+            return $this->_appPermissions;
+        }
+
+        // ##########################################
+
+        /**
+         * @return array
+         */
+        public function getFacebookConfig()
+        {
+            return [
+                'appId'       => $this->getAppId(),
+                'secret'      => $this->getAppSecret(),
+                'permissions' => $this->getAppPermissions(),
+            ];
+        }
+
+        // ##########################################
+
+        /**
+         * @param $accessToken
+         * @param bool $retry
+         * @return array
+         * @throws \Exception
+         */
+        public function getUserData($accessToken, $retry = TRUE)
+        {
+            try
+            {
+                return $this->_requestGraph('me', $accessToken);
+            }
+            catch(\Exception $e)
+            {
+                // retry in case facebook wasnt quick enough to update accessToken remotely
+                if($retry !== FALSE && strpos($e->getMessage(), 'CODE=190') !== FALSE)
+                {
+                    // wait 2 seconds and try again
+                    sleep(2);
+
+                    return $this->getUserData($accessToken, FALSE);
+                }
+
+                throw new \Exception($e->getMessage(), 500);
+            }
+        }
+
+        // ##########################################
+
+        /**
+         * @param $resource
+         * @param $accessToken
+         * @return array
+         * @throws \Exception
+         */
+        protected function _requestGraph($resource, $accessToken)
+        {
+            // make sure that we have what we need
+            if(! $resource || ! $accessToken)
+            {
+                throw new \Exception(__CLASS__ . ': Cannot request graph. Missing either $resource or $accessToken.', 500);
+            }
+
+            // request FB graph
+            $responseJson = \CURL::init("https://graph.facebook.com/{$resource}/?access_token={$accessToken}")
+                ->setReturnTransfer(TRUE)
+                ->execute();
+
+            // get array
+            $data = json_decode($responseJson, TRUE);
+
+            // handle error response
+            if(isset($data['error']))
+            {
+                $errorMetas = ['Failed graph request.'];
+
+                if(isset($data['message']))
+                {
+                    $errorMetas[] = "MSG={$data['message']}";
+                }
+
+                if(isset($data['type']))
+                {
+                    $errorMetas[] = "TYPE={$data['type']}";
+                }
+
+                if(isset($data['code']))
+                {
+                    $errorMetas[] = "CODE={$data['code']}";
+                }
+
+                if(isset($data['error_subcode']))
+                {
+                    $errorMetas[] = "SUBCODE={$data['error_subcode']}";
+                }
+
+                throw new \Exception(__CLASS__ . ': ' . join(' --> ', $errorMetas), 500);
+            }
+
+            // return data
+            return $data;
+        }
     }
-
-    // ##########################################
-
-    /**
-     * @return mixed
-     */
-    public function getAppId()
-    {
-      return $this->_appId;
-    }
-
-    // ##########################################
-
-    /**
-     * @param $appSecret
-     * @return Facebook
-     */
-    public function setAppSecret($appSecret)
-    {
-      $this->_appSecret = $appSecret;
-
-      return $this;
-    }
-
-    // ##########################################
-
-    /**
-     * @return mixed
-     */
-    public function getAppSecret()
-    {
-      return $this->_appSecret;
-    }
-
-    // ##########################################
-
-    /**
-     * @param array $appPermissions
-     * @return Facebook
-     */
-    public function setAppPermissions(array $appPermissions)
-    {
-      $this->_appPermissions = $appPermissions;
-
-      return $this;
-    }
-
-    // ##########################################
-
-    /**
-     * @return mixed
-     */
-    public function getAppPermissions()
-    {
-      return $this->_appPermissions;
-    }
-
-    // ##########################################
-
-    /**
-     * @return array
-     */
-    public function getFacebookConfig()
-    {
-      return array(
-        'appId'       => $this->getAppId(),
-        'secret'      => $this->getAppSecret(),
-        'permissions' => $this->getAppPermissions(),
-      );
-    }
-
-    // ##########################################
-
-    /**
-     * @return \Facebook
-     */
-    public function getFacebookSdk()
-    {
-      if(! $this->_facebookSdk)
-      {
-        $facebookConfig = $this->getFacebookConfig();
-        $this->_facebookSdk = new \Facebook($facebookConfig);
-      }
-
-      return $this->_facebookSdk;
-    }
-
-    // ##########################################
-
-    /**
-     * @return string
-     * @throws \Exception
-     */
-    public function getUserId()
-    {
-      try
-      {
-        return $this
-          ->getFacebookSdk()
-          ->getUser();
-      }
-      catch(\Exception $e)
-      {
-        throw new \Exception('Simplon/Facebook: Cannot access facebookUserId: ' . $e);
-      }
-    }
-
-    // ##########################################
-
-    /**
-     * @param $facebookUserId
-     * @return mixed
-     * @throws \Exception
-     */
-    public function getRemoteUserData($facebookUserId)
-    {
-      try
-      {
-        return $this
-          ->getFacebookSdk()
-          ->api('/' . $facebookUserId);
-      }
-      catch(\Exception $e)
-      {
-        throw new \Exception('Simplon/Facebook: Remote request failed:' . $e);
-      }
-    }
-
-    // ##########################################
-
-    /**
-     * @param $signedRequest
-     * @param bool $extended
-     */
-    public function authViaSignedRequest($signedRequest, $extended = FALSE)
-    {
-      // we do not want to rely on cookies
-      $_COOKIE = array();
-
-      // set signed request
-      $this
-        ->getFacebookSdk()
-        ->setSignedRequest($signedRequest);
-
-      // extend access token (only once per session)
-      if($extended === TRUE && ! isset($_SESSION['getExtendedAccessToken']))
-      {
-        $this
-          ->getFacebookSdk()
-          ->setExtendedAccessToken();
-
-        $_SESSION['getExtendedAccessToken'] = TRUE;
-      }
-    }
-
-    // ##########################################
-
-    /**
-     * @param $accessToken
-     * @param bool $extended
-     */
-    public function authViaAccessToken($accessToken, $extended = FALSE)
-    {
-      // we do not want to rely on cookies
-      $_COOKIE = array();
-
-      // set signed request
-      $this
-        ->getFacebookSdk()
-        ->setAccessToken($accessToken);
-
-      // extend access token (only once per session)
-      if($extended === TRUE && ! isset($_SESSION['getExtendedAccessToken']))
-      {
-        $this
-          ->getFacebookSdk()
-          ->setExtendedAccessToken();
-
-        $_SESSION['getExtendedAccessToken'] = TRUE;
-      }
-    }
-  }
